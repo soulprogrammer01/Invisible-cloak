b670a88a4f67eeb6e7ac7d45cc6a215cbcaeb800
Mayank Mishra <53926096+soulprogrammer01@users.noreply.github.com>
2022-08-15 22:51:54 +0530

    Update cloak.py

--- cloak.py
+++ cloak.py
@@ -1,43 +1,42 @@
-import cv2
-import numpy as np
+import cv2 # for image processing
+import numpy as np #mathematical library for image handling
 
 cap = cv2.VideoCapture(0)
-back = cv2.imread('./image.jpg')
+background = cv2.imread('./image.jpg')
 
 while cap.isOpened():
-    ret, frame = cap.read()
-
+    #caturing the live frame
+    ret, current_frame = cap.read()
     if ret:
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        #cv2.imshow("hsv", hsv)
-        red = np.uint8([[[0,0, 255]]])
-        hsv_red = cv2.cvtColor(red, cv2.COLOR_BGR2HSV),
+        #converting from rgb to hsv color space
+        hsv_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2HSV)
 
-        l_red = np.array([0, 100, 100])
-        u_red = np.array([10, 255, 255])
+        #range for lower red
+        l_red = np.array([0,120,170])
+        u_red = np.array([10,255,255])
+        mask1 = cv2.inRange(hsv_frame, l_red, u_red)
 
-        mask = cv2.inRange(hsv, l_red, u_red)
-        #cv2.imshow("mask", mask)
+        #range for lower red
+        l_red = np.array([170,120,70])
+        u_red = np.array([180,255,255])
+        mask2 = cv2.inRange(hsv_frame, l_red, u_red)
+
+        #generating the final red mask
+        red_mask = mask1 + mask2
+
+        #subsituting the red portion with backgrpound image
+        part1 = cv2.bitwise_and(background, background, mask= red_mask)
         
-        
-        #cv2.imshow("part1", part1)
-        mask=cv2.morphologyEx(mask, cv2.MORPH_OPEN,np.ones((3,3),np.uint8))
-        mask1=cv2.morphologyEx(mask, cv2.MORPH_DILATE,np.ones((3,3),np.uint8))
+        # detecting things which are not red
+        red_free = cv2.bitwise_not(red_mask)
 
-        mask1 = cv2.bitwise_not(mask)
-        part1 = cv2.bitwise_and(back, back, mask=mask)
-        part2 = cv2.bitwise_and(frame,frame , mask=mask1)
-        #cv2.imshow("part2", part2)
-
-        finalOutput=cv2.addWeighted(part1,1,part2,1,0)
+        # if cloak is not present show the current image
+        part2 = cv2.bitwise_and(current_frame, current_frame, mask= red_free)
 
 
-        cv2.imshow("cloak",finalOutput)
-
+        #final output
+        cv2.imshow("cloak", part1 + part2)
         if cv2.waitKey(5) == ord('q'):
             break
-
-
-
 cap.release()
 cv2.destroyAllWindows()
